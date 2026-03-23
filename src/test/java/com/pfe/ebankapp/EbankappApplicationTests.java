f691cb3e425c048bf7f4a48360589d24696dfae5
Hedi Nsibi <hedi.nsibi@gmail.com>
2026-03-23 10:49:53 +0100

    Initial commit - spring boot setup

---
+++ src/test/java/com/pfe/ebankapp/EbankappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfe.ebankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
